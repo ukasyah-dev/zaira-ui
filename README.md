c8df6c2d1f6e318fd0881fe7dfc09209e9da782a
Ukasyah <hi.ukasyah@gmail.com>
2025-01-12 16:17:56 +0700

    update home page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # zaira-ui
+
+A simple and elegant UI library for Svelte, designed to help you create beautiful interfaces with ease.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-screen flex-col items-center justify-center text-center">
+	<div class="w-full max-w-md">
+		<h1 class="mb-3 text-3xl font-semibold">Zaira UI</h1>
+		<p>
+			A simple and elegant UI library for Svelte, designed to help you create beautiful interfaces
+			with ease.
+		</p>
+	</div>
+</div>
