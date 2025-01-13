de80c3fb2c5597288f7127926915f05886df8fb2
Ukasyah <hi.ukasyah@gmail.com>
2025-01-13 19:04:02 +0700

    add container doc

---
+++ src/lib/components/layout/Preview.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { twJoin } from 'tailwind-merge';
+
+	let { children } = $props();
+</script>
+
+<div
+	class={twJoin(
+		'not-prose my-8 min-h-80 px-4 py-2',
+		'flex flex-col items-center justify-center',
+		'rounded-lg border border-border'
+	)}
+>
+	{@render children()}
+</div>
--- src/lib/components/layout/index.ts
+++ src/lib/components/layout/index.ts
@@ -1,2 +1,3 @@
 export { default as Container } from './Container.svelte';
+export { default as Preview } from './Preview.svelte';
 export { default as Provider } from './Provider.svelte';
